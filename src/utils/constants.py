b54d0a32604e5d69171ae1d36e22090ea4337057
chimenkamp <chimenkamp@gmail.com>
2024-06-26 13:42:02 +0200

    Update Constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -5,7 +5,7 @@ from typing import Sequence
 from htmltools import Tagifiable, Tag, MetadataNode
 from htmltools._core import ReprHtml, TagList, TagAttrDict
 
-from SpeciAL.estimation import species_estimator, species_retrieval
+from special.estimation import species_estimator, species_retrieval
 
 HTMLBody = Tagifiable | Tag | MetadataNode | ReprHtml | str | TagList | float | None | Sequence | dict[str, str | float | bool | None] | TagAttrDict
 
@@ -21,88 +21,13 @@ DEFAULT_ATTRS = {
 
 DEFAULT_SET_SIZE: int = 100
 
-ESTIMATOR_MAP = \
-    {
-        "1-gram": species_estimator.SpeciesEstimator(
-            partial(species_retrieval.retrieve_species_n_gram, n=1),
-            d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-        "2-gram": species_estimator.SpeciesEstimator(
-            partial(species_retrieval.retrieve_species_n_gram, n=2),
-                        d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-        "3-gram": species_estimator.SpeciesEstimator(
-            partial(species_retrieval.retrieve_species_n_gram, n=3),
-                        d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-        "4-gram": species_estimator.SpeciesEstimator(
-            partial(species_retrieval.retrieve_species_n_gram, n=4),
-                        d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-        "5-gram": species_estimator.SpeciesEstimator(
-            partial(species_retrieval.retrieve_species_n_gram, n=5),
-                        d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-        "trace_variants": species_estimator.SpeciesEstimator(
-            species_retrieval.retrieve_species_trace_variant,
-                        d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-
-        "2-gram_complete_log": species_estimator.SpeciesEstimator(
-            partial(species_retrieval.retrieve_species_n_gram, n=2),
-                        d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-
-        "2-gram_all_metrics": species_estimator.SpeciesEstimator(
-            partial(species_retrieval.retrieve_species_n_gram, n=2),
-                        d0=True,
-            d1=True,
-            d2=True,
-            c0=True,
-            c1=True,
-            l_n=[0.9, 0.95, 0.99],
-            step_size=DEFAULT_SET_SIZE
-        ),
-    }
+RETRIVAL_MAP = {
+    "1-gram": partial(species_retrieval.retrieve_species_n_gram, n=1),
+    "2-gram": partial(species_retrieval.retrieve_species_n_gram, n=2),
+    "3-gram": partial(species_retrieval.retrieve_species_n_gram, n=3),
+    "4-gram": partial(species_retrieval.retrieve_species_n_gram, n=4),
+    "5-gram": partial(species_retrieval.retrieve_species_n_gram, n=5),
+    "trace_variants": species_retrieval.retrieve_species_trace_variant,
+    "2-gram_complete_log": species_retrieval.retrieve_timed_activity,
+    "2-gram_all_metrics": species_retrieval.retrieve_timed_activity_exponential,
+}
